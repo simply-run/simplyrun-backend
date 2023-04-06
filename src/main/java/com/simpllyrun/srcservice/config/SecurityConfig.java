414a8afb5fc548bd19b378e9050337db3c789847
이민석 <lms0923@datasolution.kr>
2023-04-06 18:05:51 +0900

    spring SecurityConfig 임시 커밋

---
+++ src/main/java/com/simpllyrun/srcservice/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.simpllyrun.srcservice.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(request -> request
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf()
+                .disable()
+                .cors()
+                .disable()
+                .formLogin()
+                .disable()
+                .logout()
+                .disable()
+                .sessionManagement();
+
+        return http.build();
+    }
+}
