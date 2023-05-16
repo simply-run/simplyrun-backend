41715d2211cc94e9ca84afb22390a026bb904cc9
invigorating92 <84106746+invigorating92@users.noreply.github.com>
2023-05-16 14:20:31 +0900

    feat: 서비스테스트 deletePost, updatePost 추가

--- src/test/java/com/simpllyrun/srcservice/api/feed/service/PostServiceTest.java
+++ src/test/java/com/simpllyrun/srcservice/api/feed/service/PostServiceTest.java
@@ -1,12 +1,13 @@
 package com.simpllyrun.srcservice.api.feed.service;
 
-import com.simpllyrun.srcservice.api.domain.Follow;
 import com.simpllyrun.srcservice.api.domain.User;
 import com.simpllyrun.srcservice.api.feed.domain.Post;
 import com.simpllyrun.srcservice.api.feed.dto.PostDto;
 import com.simpllyrun.srcservice.api.feed.repository.PostRepository;
 import com.simpllyrun.srcservice.api.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class PostServiceTest {
@@ -33,6 +35,7 @@ class PostServiceTest {
 
     @Test
     @WithMockUser(username = "1")
+    @DisplayName("Post 생성 테스트")
     void createPost() {
         // given
         var toUser = User.builder()
@@ -45,7 +48,6 @@ class PostServiceTest {
                 .build();
         given(userRepository.findById(anyLong())).willReturn(Optional.of(toUser));
         given(postRepository.save(any())).willReturn(result);
-
         // when
         var postId = postService.createPost(postDto);
 
@@ -54,10 +56,51 @@ class PostServiceTest {
     }
 
     @Test
+    @WithMockUser(username = "1")
+    @DisplayName("Post 삭제 테스트")
     void deletePost() {
+        //given
+        var post = Post.builder()
+                .id(1L)
+                .content("content")
+                .build();
+
+        var post2 = Post.builder().id(2L).content("content2").build(); //검증 오류 확인용 post1 생성
+
+        given(postRepository.findById(anyLong())).willReturn(Optional.of(post));
+
+        // when
+        postService.deletePost(post.getId());
+
+        //then
+        verify(postRepository).delete(post); //post2 넣으면 검증 오류 발생
     }
 
-    @Test
-    void updatePost() {
+
+        @Test
+        @WithMockUser(username = "1")
+        @DisplayName("Post 수정 테스트")
+        void updatePost(){
+            //given
+            Long postId = 1L;
+            String content1 = "before change";
+
+            var post = Post.builder()
+                    .id(postId)
+                    .content(content1)
+                    .build();
+
+            given(postRepository.save(any())).willReturn(post);
+            given(postRepository.findById(anyLong())).willReturn(Optional.of(post));
+
+            System.out.println("Before-update content = " + post.getContent());
+            //when
+            String content2 = "After change";
+            postService.updatePost(postId, content2);
+            //then
+            verify(postRepository).save(post);
+            assertThat(post.getContent()).isEqualTo(content2);
+            System.out.println("After-update content = " + post.getContent());
+        }
+
     }
-}
