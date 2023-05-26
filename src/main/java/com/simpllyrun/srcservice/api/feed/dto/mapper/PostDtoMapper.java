a918174d656de45a239155d4b4361aa5806f05bf
invigorating92 <84106746+invigorating92@users.noreply.github.com>
2023-05-26 17:23:36 +0900

    feat: List<Image> postImages 추가

--- src/main/java/com/simpllyrun/srcservice/api/feed/dto/mapper/PostDtoMapper.java
+++ src/main/java/com/simpllyrun/srcservice/api/feed/dto/mapper/PostDtoMapper.java
@@ -1,17 +1,20 @@
 package com.simpllyrun.srcservice.api.feed.dto.mapper;
 
 import com.simpllyrun.srcservice.api.domain.User;
-import com.simpllyrun.srcservice.api.dto.user.UserDto;
+import com.simpllyrun.srcservice.api.feed.domain.Image;
 import com.simpllyrun.srcservice.api.feed.domain.Post;
 import com.simpllyrun.srcservice.api.feed.dto.PostDto;
 
+import java.util.List;
+
 // 나중에.. Mapper 어노테이션 적용
 public interface PostDtoMapper {
-    static Post toEntity(PostDto postDto, User user) {
+    static Post toEntity(PostDto postDto, User user, List<Image> postImages) {
         return Post.builder()
                 .categoryType(postDto.getCategory())
                 .content(postDto.getContent())
                 .user(user)
+                .postImages(postImages)
                 .build();
     }
 }
