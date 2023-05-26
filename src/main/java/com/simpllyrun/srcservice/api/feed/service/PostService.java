35752d27c2bf75fdb8072d13cdb810b1abe78701
invigorating92 <84106746+invigorating92@users.noreply.github.com>
2023-05-26 17:19:39 +0900

    feat: PostService에 List<Multiparfile> multiparfiles 파라미터 추가

--- src/main/java/com/simpllyrun/srcservice/api/feed/service/PostService.java
+++ src/main/java/com/simpllyrun/srcservice/api/feed/service/PostService.java
@@ -1,10 +1,13 @@
 package com.simpllyrun.srcservice.api.feed.service;
 
 import com.simpllyrun.srcservice.api.feed.dto.PostDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface PostService {
 
-    Long createPost(PostDto postDto);
+    Long createPost(PostDto postDto, List<MultipartFile> multipartFile);
     void deletePost(Long postId);
-    void updatePost(Long postId, String content);
+    void updatePost(Long postId, String content, List<MultipartFile> multipartFiles);
 }
