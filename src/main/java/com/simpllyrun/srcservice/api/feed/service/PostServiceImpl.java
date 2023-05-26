9064bb58579c899c03388227e553f2e6c7ff7ec4
invigorating92 <84106746+invigorating92@users.noreply.github.com>
2023-05-26 17:23:01 +0900

    feat: PostServiceImpl 이미지 저장 로직 추가

--- src/main/java/com/simpllyrun/srcservice/api/feed/service/PostServiceImpl.java
+++ src/main/java/com/simpllyrun/srcservice/api/feed/service/PostServiceImpl.java
@@ -1,6 +1,9 @@
 package com.simpllyrun.srcservice.api.feed.service;
 
+import com.simpllyrun.srcservice.api.feed.domain.Image;
 import com.simpllyrun.srcservice.api.feed.dto.PostDto;
+import com.simpllyrun.srcservice.api.feed.dto.ImageDto;
+import com.simpllyrun.srcservice.api.feed.dto.mapper.ImageDtoMapper;
 import com.simpllyrun.srcservice.api.feed.dto.mapper.PostDtoMapper;
 import com.simpllyrun.srcservice.api.feed.repository.PostRepository;
 import com.simpllyrun.srcservice.api.repository.UserRepository;
@@ -8,7 +11,10 @@ import com.simpllyrun.srcservice.util.AuthUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
@@ -17,17 +23,26 @@ public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
     private final UserRepository userRepository;
+    private final ImageService imageService;
 
     @Override
-    @Transactional
-    public Long createPost(PostDto postDto) {
-        // 이미지 저장 로직 추가 해야 함
+    public Long createPost(PostDto postDto, List<MultipartFile> multipartFiles) {
+
+        List<Image> postImages = new ArrayList<>();
         Long userId = AuthUtil.getAuthUserId();
 
+        // 이미지 저장 로직 추가
+        if (multipartFiles != null){
+            List<ImageDto> imageDtoList = imageService.uploadImage(multipartFiles);
+            for (ImageDto imageDto : imageDtoList) {
+                Image image = ImageDtoMapper.toEntity(imageDto);
+                postImages.add(image);
+            }
+        }
         var user = userRepository.findById(userId)
                 .orElseThrow(NoSuchElementException::new);
 
-        var post = postRepository.save(PostDtoMapper.toEntity(postDto, user));
+        var post = postRepository.save(PostDtoMapper.toEntity(postDto, user, postImages));
 
         return post.getId();
     }
@@ -42,9 +57,19 @@ public class PostServiceImpl implements PostService {
 
     @Override
     @Transactional
-    public void updatePost(Long postId, String content) {
+    public void updatePost(Long postId, String content, List<MultipartFile> multipartFiles) {
         var post = postRepository.findById(postId).orElseThrow(()-> new IllegalArgumentException("해당 게시물은 존재하지 않습니다."));
+        List<Image> postImages = post.getPostImages();
 
+        if (multipartFiles != null){
+            List<ImageDto> imageDtoList = imageService.uploadImage(multipartFiles);
+            for (ImageDto imageDto : imageDtoList) {
+                Image image = ImageDtoMapper.toEntity(imageDto);
+                postImages.add(image);
+            }
+        }
+
+        post.updateImage(postImages);
         post.update(content);
         postRepository.save(post);
     }
