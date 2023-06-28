e6da68f7bf0ca11e583e7156366f40e9377c46a2
invigorating92 <84106746+invigorating92@users.noreply.github.com>
2023-06-28 13:11:18 +0900

    refactor : Image -> PostImage 변경

--- src/main/java/com/simpllyrun/srcservice/api/feed/repository/PostImageRepository.java
+++ src/main/java/com/simpllyrun/srcservice/api/feed/repository/PostImageRepository.java
@@ -1,9 +1,9 @@
 package com.simpllyrun.srcservice.api.feed.repository;
 
-import com.simpllyrun.srcservice.api.feed.domain.Image;
+import com.simpllyrun.srcservice.api.feed.domain.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ImageRepository extends JpaRepository<Image, Long> {
+public interface PostImageRepository extends JpaRepository<PostImage, Long> {
 }
