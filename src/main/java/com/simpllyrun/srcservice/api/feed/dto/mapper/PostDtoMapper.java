3ffa5e8f24935086c6ac5a571d49009cbca1ba5e
invigorating92 <84106746+invigorating92@users.noreply.github.com>
2023-07-05 17:11:24 +0900

    refactor : title 추가, postImages 삭제 -> Post 엔티티의 updateImage() 메소드로 대체

--- src/main/java/com/simpllyrun/srcservice/api/feed/dto/mapper/PostDtoMapper.java
+++ src/main/java/com/simpllyrun/srcservice/api/feed/dto/mapper/PostDtoMapper.java
@@ -1,20 +1,18 @@
 package com.simpllyrun.srcservice.api.feed.dto.mapper;
 
-import com.simpllyrun.srcservice.api.feed.domain.Image;
 import com.simpllyrun.srcservice.api.feed.domain.Post;
 import com.simpllyrun.srcservice.api.feed.dto.PostDto;
 import com.simpllyrun.srcservice.api.user.domain.User;
 
-import java.util.List;
 
 // 나중에.. Mapper 어노테이션 적용
 public interface PostDtoMapper {
-    static Post toEntity(PostDto postDto, User user, List<Image> postImages) {
+    static Post toEntity(PostDto.PostRequestDto postDto, User user) {
         return Post.builder()
                 .categoryType(postDto.getCategory())
+                .title(postDto.getTitle())
                 .content(postDto.getContent())
                 .user(user)
-                .postImages(postImages)
                 .build();
     }
 }
