44d444a3f00c21bed3b4d58ece3a0c6e75ddec1a
invigorating92 <84106746+invigorating92@users.noreply.github.com>
2023-07-06 21:20:18 +0900

    feat : CommentRepository

--- src/main/java/com/simpllyrun/srcservice/api/feed/repository/CommentRepository.java
+++ src/main/java/com/simpllyrun/srcservice/api/feed/repository/CommentRepository.java
@@ -4,9 +4,15 @@ import com.simpllyrun.srcservice.api.feed.domain.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Page<Comment> findAll(Pageable pageable);
+    @Query(value = "select c from Comment c where c.post.id = :postId")
+    Page<Comment> findAllByPostId(@Param(value = "postId") Long postId, Pageable pageable);
+
+    @Query(value = "select c from Comment c where c.user.userId = :userId")
+    Page<Comment> findAllByUserId(@Param(value = "userId") String userId, Pageable pageable);
 }
