a13b39a95cdc5fcbd1cabda8bb762114b6cd9869
invigorating92 <84106746+invigorating92@users.noreply.github.com>
2023-07-06 21:21:25 +0900

    feat : CommentServiceTest

--- src/test/java/com/simpllyrun/srcservice/api/feed/service/comment/CommentServiceTest.java
+++ src/test/java/com/simpllyrun/srcservice/api/feed/service/comment/CommentServiceTest.java
@@ -6,7 +6,7 @@ import com.simpllyrun.srcservice.api.feed.domain.Comment;
 import com.simpllyrun.srcservice.api.feed.domain.Post;
 import com.simpllyrun.srcservice.api.feed.dto.CommentDto;
 import com.simpllyrun.srcservice.api.feed.repository.CommentRepository;
-import com.simpllyrun.srcservice.api.feed.repository.PostRepository;
+import com.simpllyrun.srcservice.api.feed.repository.post.PostRepository;
 import com.simpllyrun.srcservice.api.user.domain.User;
 import com.simpllyrun.srcservice.api.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -26,7 +26,6 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -48,7 +47,7 @@ class CommentServiceTest {
     @Test
     @WithMockUser(username = "1")
     @DisplayName("댓글 생성 테스트")
-    void createComment() throws JsonProcessingException {
+    void createComment(){
         //given
         var user = User.builder().id(1L).userId("khw4756").build();
         var post = Post.builder().id(1L).user(user).content("content").build();
@@ -127,45 +126,103 @@ class CommentServiceTest {
 
     }
 
+    @Nested
+    @DisplayName("댓글 단건 조회 테스트")
+    class 댓글조회테스트{
+        Comment comment = Comment.builder().id(1L).content("comment").build();
+        @Test
+        @WithMockUser(username = "1")
+        @DisplayName("댓글 조회 테스트")
+        void findComment() throws JsonProcessingException {
+            //given
+            given(commentRepository.findById(anyLong())).willReturn(Optional.of(comment));
 
-    @Test
-    @WithMockUser(username = "1")
-    @DisplayName("댓글 조회 테스트")
-    void findComment() throws JsonProcessingException {
-        //given
-        var comment = Comment.builder().id(1L).content("comment").build();
-        given(commentRepository.findById(anyLong())).willReturn(Optional.of(comment));
+            //when
+            CommentDto commentDto = commentService.findCommentById(anyLong());
+            //then
+            assertThat(commentDto.getId()).isEqualTo(1L);
+            assertThat(commentDto.getContent()).isEqualTo("comment");
+            System.out.println(objectMapper.writeValueAsString(commentDto));
+        }
 
-        //when
-        CommentDto commentDto = commentService.findCommentById(anyLong());
-        //then
-        assertThat(commentDto.getId()).isEqualTo(1L);
-        assertThat(commentDto.getContent()).isEqualTo("comment");
-        System.out.println(objectMapper.writeValueAsString(commentDto));
+        @Test
+        @DisplayName("NoSuchElementException 테스트")
+        void exceptionTest(){
+            given(commentRepository.findById(1L)).willReturn(Optional.empty());
+            assertThatCode(()->commentService.findCommentById(1L))
+                    .isInstanceOf(NoSuchElementException.class);
+
+        }
+
     }
 
-    @Test
-    @WithMockUser(username = "1")
+    @Nested
     @DisplayName("댓글 전체 조회 테스트")
-    void findAllComment() throws JsonProcessingException {
-        //given
+    class 댓글_전체_조회{
+
+        User user = User.builder().id(1L).userId("khw").build();
+        Post post = Post.builder().id(1L).content("content").build();
         PageRequest pageRequest = PageRequest.of(0, 10);
-        var comment1 = Comment.builder().id(1L).content("comment").build();
-        var comment2 = Comment.builder().id(2L).content("comment").build();
-        var comment3 = Comment.builder().id(3L).content("comment").build();
+        Comment comment1 = Comment.builder().id(1L).content("comment1").user(user).post(post).build();
+        Comment comment2 = Comment.builder().id(2L).content("comment2").user(user).post(post).build();
+        Comment comment3 = Comment.builder().id(3L).content("comment3").user(user).post(post).build();
         List<Comment> commentList = new ArrayList<>();
-        commentList.add(comment1);
-        commentList.add(comment2);
-        commentList.add(comment3);
 
-        PageImpl<Comment> commentPageImpl = new PageImpl<>(commentList, pageRequest, commentList.size());
-        given(commentRepository.findAll(pageRequest)).willReturn(commentPageImpl);
+        @Test
+        @WithMockUser(username = "1")
+        @DisplayName("postId 게시글의 전체 댓글 조회 테스트")
+        void findAllComment() throws JsonProcessingException {
+            //given
+            commentList.add(comment1);
+            commentList.add(comment2);
+            commentList.add(comment3);
 
-        //when
-        Page<Comment> commentPage = commentService.findAll(pageRequest);
-        //then
-        System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(commentPage));
-        assertThat(commentPage.getTotalElements()).isEqualTo(commentPageImpl.getTotalElements());
+            PageImpl<Comment> commentPageImpl = new PageImpl<>(commentList, pageRequest, commentList.size());
+            given(commentRepository.findAllByPostId(1L, pageRequest)).willReturn(commentPageImpl);
+            given(postRepository.findById(anyLong())).willReturn(Optional.of(post));
+
+            //when
+            Page<Comment> commentPage = commentService.findAllByPostId(1L, pageRequest);
+            //then
+            System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(commentPage));
+            assertThat(commentPage.getTotalElements()).isEqualTo(commentPageImpl.getTotalElements());
+        }
+
+        @Test
+        @DisplayName("postId 게시글 전체 댓글 조회 NoSuchElementException 테스트")
+        void postIdExceptionTest(){
+            given(postRepository.findById(1L)).willReturn(Optional.empty());
+            assertThatCode(()->commentService.findAllByPostId(1L, pageRequest))
+                    .isInstanceOf(NoSuchElementException.class);
+        }
+
+        @Test
+        @WithMockUser(username = "1")
+        @DisplayName("userId 유저가 작성한 댓글 전체 조회 테스트")
+        void findAllCommentByUserId() throws JsonProcessingException {
+            //given
+            commentList.add(comment1);
+            commentList.add(comment2);
+            commentList.add(comment3);
+
+            PageImpl<Comment> commentPageImpl = new PageImpl<>(commentList, pageRequest, commentList.size());
+            given(commentRepository.findAllByUserId("khw", pageRequest)).willReturn(commentPageImpl);
+            given(userRepository.findByUserId(anyString())).willReturn(Optional.of(user));
+
+            //when
+            Page<Comment> commentPage = commentService.findAllByUserId("khw", pageRequest);
+            //then
+            System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(commentPage));
+            assertThat(commentPage.getTotalElements()).isEqualTo(commentPageImpl.getTotalElements());
+        }
+
+        @Test
+        @DisplayName("userId 유저의 전체 댓글 조회 NoSuchElementException 테스트")
+        void userIdExceptionTest(){
+            given(userRepository.findByUserId("khw")).willReturn(Optional.empty());
+            assertThatCode(()->commentService.findAllByUserId("khw", pageRequest))
+                    .isInstanceOf(NoSuchElementException.class);
+        }
     }
 
 }
